a3ece5467c4206f96cf535edf92eba2676c3949e
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-02 00:53:08 -0500

    Initial commit

---
+++ src/main/java/com/vet/hc/api/VetManagerHcApiApplication.java
@@ -0,0 +1,8 @@
+package com.vet.hc.api;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("api/v0")
+public class VetManagerHcApiApplication extends Application {
+}
