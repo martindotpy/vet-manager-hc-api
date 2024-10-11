8d7f663afbc73d02febbf7e68b4a1e1fb34ec850
Martin Alexander Ramos Yampufe <alexramosyampufe@hotmail.com>
2024-10-11 13:10:53 -0500

    FEAT: Add port placeholders for use cases

---
+++ src/main/java/com/vet/hc/api/user/application/port/in/GetCurrentUserPort.java
@@ -0,0 +1,10 @@
+package com.vet.hc.api.user.application.port.in;
+
+/**
+ * Port for get the current user (logged in user).
+ */
+public interface GetCurrentUserPort {
+    /**
+     * Get the current user.
+     */
+}
