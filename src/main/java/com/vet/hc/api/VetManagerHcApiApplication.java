1cdb7c71809fd6d56951826cd65c9ad96945a9e6
Martin Alexander Ramos Yampufe <alexramosyampufe@hotmail.com>
2024-10-18 10:20:42 -0500

    DOC: Add javadoc in application

--- src/main/java/com/vet/hc/api/VetManagerHcApiApplication.java
+++ src/main/java/com/vet/hc/api/VetManagerHcApiApplication.java
@@ -3,6 +3,9 @@ package com.vet.hc.api;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+/**
+ * Configures the base path for the REST API.
+ */
 @ApplicationPath("api/v0")
-public class VetManagerHcApiApplication extends Application {
+public class VetManagerHCApiApplication extends Application {
 }
