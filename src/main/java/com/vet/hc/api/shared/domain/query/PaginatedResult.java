3ccf0cace0e02d9a72f4207eec003c754570706f
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-21 21:03:12 -0500

    FEAT: Improve query classes

---
+++ src/main/java/com/vet/hc/api/shared/domain/query/PaginatedResult.java
@@ -0,0 +1,13 @@
+package com.vet.hc.api.shared.domain.query;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class PaginatedResult<T> extends Response<T> {
+    private final int page;
+    private final int pageSize;
+    private final int totalPages;
+    private final long totalElements;
+}
--- src/main/java/com/vet/hc/api/shared/domain/query/Result.java
+++ src/main/java/com/vet/hc/api/shared/domain/query/Result.java
@@ -1,5 +1,7 @@
 package com.vet.hc.api.shared.domain.query;
 
+import java.util.Objects;
+
 /**
  * The response of the query.
  *
@@ -7,7 +9,7 @@ package com.vet.hc.api.shared.domain.query;
  * It can be only a successful result or a failure.
  * </p>
  */
-public final class Result<S extends SuccessfulResponse, E extends FailureResponse> {
+public final class Result<S, E extends Failure> {
     private final S success;
     private final E error;
 
@@ -19,24 +21,37 @@ public final class Result<S extends SuccessfulResponse, E extends FailureRespons
     /**
      * Create a successful result.
      *
-     * @param <S>     the type of the successful response
-     * @param <E>     the type of the failure response
-     * @param success the successful response
+     * @param <S>     the type of the successful response.
+     * @param <E>     the type of the failure response.
+     * @param success the successful response.
      * @return the result
      */
-    public static <S extends SuccessfulResponse, E extends FailureResponse> Result<S, E> success(S success) {
+    public static <S, E extends Failure> Result<S, E> success(S success) {
+        Objects.requireNonNull(success, "The success response must not be null");
         return new Result<>(success, null);
+    }
+
+    /**
+     * Create an empty successful result.
+     *
+     * @param <S> the type of the successful response.
+     * @param <E> the type of the failure response.
+     * @return the result
+     */
+    public static <S, E extends Failure> Result<S, E> success() {
+        return new Result<>(null, null);
     }
 
     /**
      * Create a failure result.
      *
-     * @param <S>   the type of the successful response
-     * @param <E>   the type of the failure response
-     * @param error the failure response
+     * @param <S>   the type of the successful response.
+     * @param <E>   the type of the failure response.
+     * @param error the failure response.
      * @return the result
      */
-    public static <S extends SuccessfulResponse, E extends FailureResponse> Result<S, E> failure(E error) {
+    public static <S, E extends Failure> Result<S, E> failure(E error) {
+        Objects.requireNonNull(error, "The error response must not be null");
         return new Result<>(null, error);
     }
 
@@ -49,14 +64,10 @@ public final class Result<S extends SuccessfulResponse, E extends FailureRespons
     }
 
     public boolean isSuccess() {
-        return success != null;
+        return success != null || error == null;
     }
 
     public boolean isFailure() {
         return error != null;
-    }
-
-    public Response getResponse() {
-        return isSuccess() ? success : error;
     }
 }
