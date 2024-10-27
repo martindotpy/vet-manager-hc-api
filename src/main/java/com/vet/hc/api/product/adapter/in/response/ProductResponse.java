46d66e2dc1eff70a04b7b7bf90b769d30c7990e2
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-27 00:13:00 -0500

    FEAT: Add product adapter in structure

---
+++ src/main/java/com/vet/hc/api/product/adapter/in/response/ProductResponse.java
@@ -0,0 +1,12 @@
+package com.vet.hc.api.product.adapter.in.response;
+
+import com.vet.hc.api.shared.domain.query.Response;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Response object for a product.
+ */
+@SuperBuilder
+public class ProductResponse extends Response<ProductDto> {
+}
---
+++ src/main/java/com/vet/hc/api/product/adapter/in/validator/PriceExtendedValidation.java
@@ -0,0 +1,9 @@
+package com.vet.hc.api.product.adapter.in.validator;
+
+/**
+ * Extended validation group for price.
+ *
+ * @see jakarta.validation.GroupSequence
+ */
+public interface PriceExtendedValidation {
+}
