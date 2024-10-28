257ff9fb621ec8e323425396a86b75cc8aaaed63
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-27 23:03:32 -0500

    FEAT: Add command into client module

--- src/main/java/com/vet/hc/api/client/domain/command/UpdateClientCommand.java
+++ src/main/java/com/vet/hc/api/client/domain/command/UpdateClientCommand.java
@@ -1,9 +1,20 @@
 package com.vet.hc.api.client.domain.command;
 
+import com.vet.hc.api.client.domain.enums.IdentificationType;
 import com.vet.hc.api.shared.domain.command.Command;
+
+import lombok.Builder;
+import lombok.Getter;
 
 /**
  * Command to update the data client.
  */
+@Getter
+@Builder
 public class UpdateClientCommand implements Command {
+    private String firstName;
+    private String lastName;
+    private String identification;
+    private IdentificationType identificationType;
+    private String address;
 }
--- src/main/java/com/vet/hc/api/client/domain/command/UpdateClientPhonesCommand.java
+++ src/main/java/com/vet/hc/api/client/domain/command/UpdateClientPhonesCommand.java
@@ -1,9 +1,33 @@
 package com.vet.hc.api.client.domain.command;
 
+import java.util.Set;
+
+import com.vet.hc.api.client.domain.model.ClientPhone;
 import com.vet.hc.api.shared.domain.command.Command;
+
+import lombok.Builder;
+import lombok.Getter;
 
 /**
  * Command to update the client phones'.
+ *
+ * <p>
+ * If a phone is removed from the set, it will be deleted from the database.
+ * </p>
+ * <p>
+ * If a phone is added to the set without an ID, it will be inserted into the
+ * database.
+ * </p>
+ * <p>
+ * If a phone is added to the set with an ID, it will be updated in the
+ * database.
+ * </p>
+ * <p>
+ * If the client has no phones, the phones set will be empty.
+ * </p>
  */
+@Getter
+@Builder
 public class UpdateClientPhonesCommand implements Command {
+    private Set<ClientPhone> phones;
 }
