47aaf7725c55b375dd07373b0ebeb6b5387a1b10
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-27 21:16:20 -0500

    FEAT: Add mappers

---
+++ src/main/java/com/vet/hc/api/product/adapter/in/mapper/UpdateProductMapper.java
@@ -0,0 +1,20 @@
+package com.vet.hc.api.product.adapter.in.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.vet.hc.api.product.adapter.in.request.UpdateProductRequest;
+import com.vet.hc.api.product.domain.command.UpdateProductCommand;
+
+@Mapper
+public interface UpdateProductMapper {
+    UpdateProductMapper INSTANCE = Mappers.getMapper(UpdateProductMapper.class);
+
+    /**
+     * Maps a {@link UpdateProductRequest} to a {@link UpdateProductCommand}.
+     *
+     * @param request the request to map.
+     * @return the mapped command
+     */
+    UpdateProductCommand toCommand(UpdateProductRequest request);
+}
