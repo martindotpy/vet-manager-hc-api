ca8be993a99798fe4e40e4d81b0192edd1fddc30
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-29 20:41:30 -0500

    FEAT: Add jackson module to use java api time

--- src/main/java/com/vet/hc/api/shared/adapter/out/bean/CustomObjectMapper.java
+++ src/main/java/com/vet/hc/api/shared/adapter/out/bean/CustomObjectMapper.java
@@ -3,6 +3,7 @@ package com.vet.hc.api.shared.adapter.out.bean;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 /**
  * Custom object mapper for mapping the objects to JSON and vice versa.
@@ -11,5 +12,7 @@ public class CustomObjectMapper extends ObjectMapper {
     public CustomObjectMapper() {
         this.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         this.setSerializationInclusion(Include.CUSTOM);
+        this.findAndRegisterModules();
+        this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
