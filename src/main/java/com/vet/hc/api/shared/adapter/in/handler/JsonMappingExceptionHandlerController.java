64324a32e692bd4b36043b377a9816e0d86b9201
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-29 20:41:53 -0500

    FEAT: Add handlers for exceptions

--- src/main/java/com/vet/hc/api/shared/adapter/in/handler/JsonMappingExceptionHandlerController.java
+++ src/main/java/com/vet/hc/api/shared/adapter/in/handler/JsonMappingExceptionHandlerController.java
@@ -1,7 +1,6 @@
 package com.vet.hc.api.shared.adapter.in.handler;
 
-import com.fasterxml.jackson.databind.JsonMappingException.Reference;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.vet.hc.api.shared.domain.query.FailureResponse;
 
 import jakarta.ws.rs.core.Response;
@@ -10,21 +9,26 @@ import jakarta.ws.rs.ext.Provider;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Handles the {@link MismatchedInputException} exception.
+ * Handles the {@link JsonMappingException} exception.
  */
 @Slf4j
 @Provider
-public class MismatchedInputExceptionHandlerController implements ExceptionMapper<MismatchedInputException> {
+public class JsonMappingExceptionHandlerController implements ExceptionMapper<JsonMappingException> {
     @Override
-    public Response toResponse(MismatchedInputException exception) {
+    public Response toResponse(JsonMappingException exception) {
         StringBuilder messageSb = new StringBuilder();
         Response.Status status;
-        Reference reference = exception.getPath().getFirst();
 
-        if (reference != null) {
+        if (exception.getPath() != null) {
+            JsonMappingException.Reference reference = exception.getPath().get(0);
             messageSb.append("El tipo de dato introducido en el campo `")
                     .append(reference.getFieldName())
-                    .append("` no es válido");
+                    .append("` en la línea: ")
+                    .append(exception.getLocation().getLineNr())
+                    .append(", columna: ")
+                    .append(exception.getLocation().getColumnNr())
+                    .append(" no es válido");
+
             status = Response.Status.BAD_REQUEST;
         } else {
             messageSb.append("El tipo de dato introducido no es válido");
--- src/main/java/com/vet/hc/api/shared/adapter/in/handler/UnsupportedOperationExceptionHandlerController.java
+++ src/main/java/com/vet/hc/api/shared/adapter/in/handler/UnsupportedOperationExceptionHandlerController.java
@@ -5,14 +5,18 @@ import com.vet.hc.api.shared.domain.query.FailureResponse;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles the {@link UnsupportedOperationException} exception.
  */
+@Slf4j
 @Provider
 public class UnsupportedOperationExceptionHandlerController implements ExceptionMapper<UnsupportedOperationException> {
     @Override
     public Response toResponse(UnsupportedOperationException exception) {
+        log.error("Operación no soportada: {}", exception.getMessage(), exception);
+
         return Response.status(Response.Status.NOT_IMPLEMENTED)
                 .entity(FailureResponse.builder()
                         .message("Operación no soportada")
