d84e2ea5c8eb25a67e5362074546b5ec35b97c1a
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-10-29 22:36:39 -0500

    FEAT: Export excel clients

--- src/main/java/com/vet/hc/api/client/application/dto/ClientDto.java
+++ src/main/java/com/vet/hc/api/client/application/dto/ClientDto.java
@@ -1,6 +1,7 @@
 package com.vet.hc.api.client.application.dto;
 
 import com.vet.hc.api.client.domain.enums.IdentificationType;
+import com.vet.hc.api.shared.domain.spanish.SpanishPropertyName;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,9 +18,14 @@ import lombok.NoArgsConstructor;
 public class ClientDto {
     private Long id;
 
+    @SpanishPropertyName("Nombre")
     private String firstName;
+    @SpanishPropertyName("Apellido")
     private String lastName;
+    @SpanishPropertyName("Identificación")
     private String identification;
+    @SpanishPropertyName("Tipo de identificación")
     private IdentificationType identificationType;
+    @SpanishPropertyName("Dirección")
     private String address;
 }
---
+++ src/main/java/com/vet/hc/api/shared/adapter/out/service/GenerateExcelFromTableService.java
@@ -0,0 +1,48 @@
+package com.vet.hc.api.shared.adapter.out.service;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.vet.hc.api.shared.application.port.out.GenerateExcelFromTablePort;
+
+/**
+ * Service for generating an Excel file from a table.
+ */
+public class GenerateExcelFromTableService<T> implements GenerateExcelFromTablePort<T> {
+    @Override
+    public void generateExcel(OutputStream outputStream, String sheetName, List<T> data, Class<T> clazz) {
+        try (Workbook workbook = new XSSFWorkbook()) {
+            Sheet sheet = workbook.createSheet(sheetName);
+
+            Row headerRow = sheet.createRow(0);
+
+            Field[] fields = clazz.getDeclaredFields();
+
+            for (int i = 0; i < fields.length; i++) {
+                headerRow.createCell(i).setCellValue(fields[i].getName());
+            }
+
+            for (int i = 0; i < data.size(); i++) {
+                Row row = sheet.createRow(i + 1);
+
+                for (int j = 0; j < fields.length; j++) {
+                    fields[j].setAccessible(true);
+                    row.createCell(j).setCellValue(fields[j].get(data.get(i)).toString());
+                }
+            }
+
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Error generating Excel file", e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Error accessing field", e);
+        }
+    }
+}
