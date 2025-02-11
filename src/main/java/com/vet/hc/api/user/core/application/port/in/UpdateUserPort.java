70c436b8185ab46ef2cff0218c8001631175ff2b
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-11 16:20:47 -0500

    REFACTOR: Remove dependency on UpdateEntityPort in UpdateUserPort and define update method directly

--- src/main/java/com/vet/hc/api/user/core/application/port/in/UpdateUserPort.java
+++ src/main/java/com/vet/hc/api/user/core/application/port/in/UpdateUserPort.java
@@ -1,7 +1,6 @@
 package com.vet.hc.api.user.core.application.port.in;
 
 import com.vet.hc.api.auth.core.application.dto.JwtDto;
-import com.vet.hc.api.shared.application.port.in.UpdateEntityPort;
 import com.vet.hc.api.shared.domain.result.Result;
 import com.vet.hc.api.user.core.application.dto.UserDto;
 import com.vet.hc.api.user.core.domain.failure.UserFailure;
@@ -10,7 +9,15 @@ import com.vet.hc.api.user.core.domain.payload.UpdateUserPayload;
 /**
  * Update user port.
  */
-public interface UpdateUserPort extends UpdateEntityPort<UserDto, UserFailure, UpdateUserPayload> {
+public interface UpdateUserPort {
+    /**
+     * Update a user.
+     *
+     * @param payload the payload.
+     * @return the result.
+     */
+    Result<UserDto, UserFailure> update(UpdateUserPayload payload);
+
     /**
      * Update the current user.
      *
