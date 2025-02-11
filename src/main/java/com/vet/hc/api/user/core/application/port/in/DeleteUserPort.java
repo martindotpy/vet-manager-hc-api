b325729fced60ec3c38cfa43e89767dd353060cb
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-11 16:20:17 -0500

    REFACTOR: Remove unused CRUD controller shortcuts and related entity ports; add EmailSender service and port

--- src/main/java/com/vet/hc/api/user/core/application/port/in/DeleteUserPort.java
+++ src/main/java/com/vet/hc/api/user/core/application/port/in/DeleteUserPort.java
@@ -1,10 +1,17 @@
 package com.vet.hc.api.user.core.application.port.in;
 
-import com.vet.hc.api.shared.application.port.in.DeleteEntityPort;
+import com.vet.hc.api.shared.domain.result.Result;
 import com.vet.hc.api.user.core.domain.failure.UserFailure;
 
 /**
  * Delete user port.
  */
-public interface DeleteUserPort extends DeleteEntityPort<UserFailure, Long> {
+public interface DeleteUserPort {
+    /**
+     * Delete a user.
+     *
+     * @param id the id of the user to delete.
+     * @return the result of the operation.
+     */
+    Result<Void, UserFailure> deleteById(Long id);
 }
--- src/main/java/com/vet/hc/api/user/core/application/port/in/UpdateUserProfileImagePort.java
+++ src/main/java/com/vet/hc/api/user/core/application/port/in/UpdateUserProfileImagePort.java
@@ -1,7 +1,6 @@
 package com.vet.hc.api.user.core.application.port.in;
 
 import com.vet.hc.api.auth.core.application.dto.JwtDto;
-import com.vet.hc.api.shared.application.port.in.UpdateEntityPort;
 import com.vet.hc.api.shared.domain.result.Result;
 import com.vet.hc.api.user.core.application.dto.UserDto;
 import com.vet.hc.api.user.core.domain.failure.UserFailure;
@@ -10,8 +9,15 @@ import com.vet.hc.api.user.core.domain.payload.UpdateUserProfileImagePayload;
 /**
  * Update user profile image port.
  */
-public interface UpdateUserProfileImagePort
-        extends UpdateEntityPort<UserDto, UserFailure, UpdateUserProfileImagePayload> {
+public interface UpdateUserProfileImagePort {
+    /**
+     * Update user profile image.
+     *
+     * @param payload the payload.
+     * @return the result.
+     */
+    Result<UserDto, UserFailure> update(UpdateUserProfileImagePayload payload);
+
     /**
      * Update the current user.
      *
