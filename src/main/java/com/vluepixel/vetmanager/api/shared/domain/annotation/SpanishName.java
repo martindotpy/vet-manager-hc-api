d5112e5fd8502075fafaaaacb5451863b733a8e2
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-13 00:00:40 -0500

    feat: Enhance error handling in repository layer and add SpanishName annotation retention policy

--- src/main/java/com/vluepixel/vetmanager/api/shared/domain/annotation/SpanishName.java
+++ src/main/java/com/vluepixel/vetmanager/api/shared/domain/annotation/SpanishName.java
@@ -2,6 +2,8 @@ package com.vluepixel.vetmanager.api.shared.domain.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -9,6 +11,7 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Target({ ElementType.TYPE, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface SpanishName {
     String value();
 }
