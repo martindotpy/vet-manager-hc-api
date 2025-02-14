821089afc02724b2b0abc1c63a117d29bc46a46d
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-14 02:35:03 -0500

    feat: Add medical history

--- src/main/java/com/vluepixel/vetmanager/api/patient/core/application/dto/PatientDto.java
+++ src/main/java/com/vluepixel/vetmanager/api/patient/core/application/dto/PatientDto.java
@@ -1,9 +1,11 @@
 package com.vluepixel.vetmanager.api.patient.core.application.dto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import com.vluepixel.vetmanager.api.client.core.application.dto.ClientDto;
 import com.vluepixel.vetmanager.api.patient.core.domain.enums.PatientGender;
+import com.vluepixel.vetmanager.api.patient.medicalhistory.application.dto.MedicalHistoryDto;
 import com.vluepixel.vetmanager.api.patient.race.application.dto.RaceDto;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +29,7 @@ public final class PatientDto {
     private PatientGender gender;
     private String characteristics;
     private boolean deceased;
+    private List<MedicalHistoryDto> histories;
 
     private RaceDto race;
     private ClientDto owner;
