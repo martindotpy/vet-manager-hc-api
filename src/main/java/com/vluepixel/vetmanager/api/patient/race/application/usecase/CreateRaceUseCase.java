49b090a3428e78b25805ebbc78080824c990ba6a
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-13 22:18:30 -0500

    feat: Add race management functionality with create, update, delete, and find operations

---
+++ src/main/java/com/vluepixel/vetmanager/api/patient/race/application/usecase/CreateRaceUseCase.java
@@ -0,0 +1,38 @@
+package com.vluepixel.vetmanager.api.patient.race.application.usecase;
+
+import org.slf4j.MDC;
+
+import com.vluepixel.vetmanager.api.patient.race.application.dto.RaceDto;
+import com.vluepixel.vetmanager.api.patient.race.application.mapper.RaceMapper;
+import com.vluepixel.vetmanager.api.patient.race.application.port.in.CreateRacePort;
+import com.vluepixel.vetmanager.api.patient.race.domain.repository.RaceRepository;
+import com.vluepixel.vetmanager.api.patient.race.domain.request.CreateRaceRequest;
+import com.vluepixel.vetmanager.api.shared.application.annotation.UseCase;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Create race use case.
+ */
+@Slf4j
+@UseCase
+@RequiredArgsConstructor
+public class CreateRaceUseCase implements CreateRacePort {
+    private final RaceRepository raceRepository;
+    private final RaceMapper raceMapper;
+
+    @Override
+    @Transactional
+    public RaceDto create(CreateRaceRequest request) {
+        MDC.put("operationId", "Race name " + request.getName());
+        log.info("Creating race");
+
+        // Save the race
+        var newRace = raceMapper.fromRequest(request).build();
+        newRace = raceRepository.save(newRace);
+
+        return raceMapper.toDto(newRace);
+    }
+}
