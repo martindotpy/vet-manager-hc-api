43de2740b22b095e83aff22d960d5b44d3e69ae0
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-17 16:52:41 -0500

    feat: Change updateById method to use PathVariable for user ID

--- src/main/java/com/vluepixel/vetmanager/api/user/core/adapter/in/controller/UserProfileImageController.java
+++ src/main/java/com/vluepixel/vetmanager/api/user/core/adapter/in/controller/UserProfileImageController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -103,7 +104,7 @@ public class UserProfileImageController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserResponse> updateById(
             @RequestParam MultipartFile imageFile,
-            @RequestParam Long id) throws IOException {
+            @PathVariable Long id) throws IOException {
         validate(EnumValidation.of(
                 ImageMimeType.class,
                 imageFile.getContentType(),
