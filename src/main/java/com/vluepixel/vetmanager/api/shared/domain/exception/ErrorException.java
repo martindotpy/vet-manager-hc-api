c0aec5b0a6c44e80f604cdd9fe872618d6cfe277
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-02-17 23:45:14 -0500

    refactor: Remove SpanishName annotation and improve exception messages

--- src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/ErrorException.java
+++ src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/ErrorException.java
@@ -1,5 +1,8 @@
 package com.vluepixel.vetmanager.api.shared.domain.exception;
 
+/**
+ * Error exception.
+ */
 public abstract class ErrorException extends RuntimeException {
     public abstract int getStatus();
 }
--- src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/InternalServerErrorException.java
+++ src/main/java/com/vluepixel/vetmanager/api/shared/domain/exception/InternalServerErrorException.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public final class InternalServerErrorException extends ErrorException {
-    private final String message = "Internal server error";
     private final int status = 500;
     private final Throwable cause;
 
